117967ef5de622d5f55a0f998f84b05ae38cf5ee
fazriegi <fazriegi80@gmail.com>
2023-09-15 14:46:31 +0700

    add load env

--- main.go
+++ main.go
@@ -4,9 +4,17 @@ import (
 	"log"
 	"task-5-pbi-btpns-Fazri_Egi_Ramadhan/database"
 	"task-5-pbi-btpns-Fazri_Egi_Ramadhan/router"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Failed to load .env file: %w", err)
+	}
+
 	if err := database.Open(); err != nil {
 		log.Fatal(err)
 	}
